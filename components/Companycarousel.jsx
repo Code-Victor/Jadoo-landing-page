5f8489ab47349f12d4e96d9a6d1fd8ee159e940e
Code-Victor <oluwaborihamzat@gmail.com>
2022-04-27 12:17:44 +0100

    created carousels.jsx

---
+++ components/Companycarousel.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+const companyLogos = [
+  "./images/image 27.png",
+  "./images/image 28.png",
+  "./images/image 29.png",
+  "./images/image 30.png",
+  "./images/image 31.png",
+];
+function Companycarousel() {
+  return (
+    <div className="container mx-auto mt-2 md:mt-4 px-4 lg:px-0 flex w-full justify-around flex-wrap">
+      {companyLogos.map((logo, i) => {
+        return (
+          <div className="grid place-items-center h-14 bg-white rounded-lg filter-hover ">
+            <img src={logo} alt="company" />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Companycarousel;
