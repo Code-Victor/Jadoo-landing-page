feb1e698da67c2f20adec0a49decf86ec09089cd
Code-Victor <oluwaborihamzat@gmail.com>
2022-04-27 12:29:58 +0100

    exports all the components in component dir

---
+++ components/index.jsx
@@ -0,0 +1,11 @@
+export {default as NavBar} from './navbar.jsx';
+export {default as Hero} from './hero.jsx';
+export {default as Section} from './Section'
+export {default as Category} from './Category'
+export {default as Destination} from './Destination'
+export {default as BookCard} from './BookCard'
+export {default as Testimonials} from './Testimonials'
+export {default as CompanyCarousel} from './Companycarousel'
+export {default as Subscribe} from './Subscribe'
+export {default as Footer} from './Footer'
+export {default as TripStep} from './TripStep'
